ab8ade20c36a83e60ef456a76c16a2124b2bdda7
ali najmaldin <ali.najmaldin@gmail.com>
2020-01-24 00:57:02 -0500

    added dynamic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,20 @@
 // Check off specific TODOS by clicking
 
-$('li').on('click', function(){
+$('ul').on('click', 'li', function(){
     $(this).toggleClass('done')
+})
+
+$('ul').on('click', 'span', function(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove()
+        }
+    )
+    event.stopPropagation()
+})
+
+$("input[type='text']").on('keypress',  function(event){
+    if(event.which == 13){
+        $('ul').append('<li><span>X</span> ' + $(this).val() + '</li>')
+        $(this).val('')
+    }
 })
